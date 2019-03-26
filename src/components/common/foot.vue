896eca7a003e575223d67529d3660bb37abbc54c
lovecross <lovecrossyoutt@hotmail.com>
2019-03-26 23:30:27 +0800

    xx

--- src/components/common/foot.vue
+++ src/components/common/foot.vue
@@ -5,7 +5,7 @@
         <img src="@/imgs/logo.png" class="foot_logo" alt="">
       </div>
       <div class="right  c-white font-12 line-h-20">
-        <span>北京汇格信息科技有限公司有限公司 COPYRIGHT 2018-2019 . ALL RIGHTS RESERVED | 京ICP备19007689号  </span>
+        <span>北京汇格信息科技有限公司 COPYRIGHT 2018-2019 . ALL RIGHTS RESERVED | 京ICP备19007689号  </span>
       </div>
     </div>
   </footer>
