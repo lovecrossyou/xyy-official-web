c977a9fbcedae1f80dcb0185954bd03ba71b4cfd
xiannvrong <1796940658@qq.com>
2019-04-02 14:28:12 +0800

    xx

--- src/components/index/model-1.vue
+++ src/components/index/model-1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-1 container  flex">
-    <div class="width50 flex1" style="background:url(http://qnimage.xiteng.com/datu.jpg) 100% 100% no-repeat"></div>
-    <div class="width50 flex1" style="background:url(http://qnimage.xiteng.com/datu2.jpg) 100% 100% no-repeat"></div>
+    <div class="width50 flex1" style="background:url(http://qnimage.xiteng.com/shiming.jpg) 100% 100% no-repeat"></div>
+    <div class="width50 flex1" style="background:url(http://qnimage.xiteng.com/chejina.jpg) 100% 100% no-repeat"></div>
   </div>
 </template>
 
